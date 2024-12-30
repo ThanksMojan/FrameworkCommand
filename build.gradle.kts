4b80f1ad214f8c6565bcafcd7ce11e00a149f342
ThanksMojan <francisco.soportes@gmail.com>
2024-12-29 22:33:29 -0300

    implemented jitpack.io

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    //implemented jitpack.io
     implementation(kotlin("stdlib"))
     implementation ("com.github.ThanksMojan:FrameworkCommand:Tag")
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
